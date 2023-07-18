8738cbf6bb1dbfdf6d937d33b969f543a6b98b4e
Developer20202021 <mahadikaushik8888@gmail.com>
2023-07-18 21:11:35 +0600

    Log In Page

---
+++ lib/Screen/FrontScreen/LogInScreen.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:lottie/lottie.dart';
+
+
+class LogInScreen extends StatefulWidget {
+  const LogInScreen({super.key});
+
+  @override
+  State<LogInScreen> createState() => _LogInScreenState();
+}
+
+class _LogInScreenState extends State<LogInScreen> {
+  TextEditingController myEmailController = TextEditingController();
+  TextEditingController myPassController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+
+    FocusNode myFocusNode = new FocusNode();
+ 
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.purple),
+        leading: IconButton(onPressed: (){}, icon: Icon(Icons.chevron_left)),
+        title: const Text("Log In", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),
+        backgroundColor: Colors.transparent,
+        bottomOpacity: 0.0,
+        elevation: 0.0,
+        
+      ),
+      body: Container(
+
+        child:  CustomPaint(
+          painter: CurvePainter(),
+
+     
+              
+            
+            
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+            
+                    
+                    Center(
+                      child: Lottie.asset(
+                      'lib/images/animation_lk8fkoa8.json',
+                        fit: BoxFit.cover,
+                        width: 200,
+                        height: 200
+                      ),
+                    ),
+            
+            
+            
+                    TextField(
+                      focusNode: myFocusNode,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Enter Email',
+                           labelStyle: TextStyle(
+              color: myFocusNode.hasFocus ? Colors.purple: Colors.black
+                  ),
+                          hintText: 'Enter Your Email',
+            
+                          //  enabledBorder: OutlineInputBorder(
+                          //       borderSide: BorderSide(width: 3, color: Colors.greenAccent),
+                          //     ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(width: 3, color: Colors.purple),
+                              ),
+                              errorBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    width: 3, color: Color.fromARGB(255, 66, 125, 145)),
+                              ),
+                          
+                          
+                          ),
+                      controller: myEmailController,
+                    ),
+            
+            
+            
+            
+                    SizedBox(
+                      height: 20,
+                    ),
+            
+            
+            
+            
+            
+                    TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Enter Password',
+                           labelStyle: TextStyle(
+              color: myFocusNode.hasFocus ? Colors.purple: Colors.black
+                  ),
+                          hintText: 'Enter Your Password',
+                          //  enabledBorder: OutlineInputBorder(
+                          //     borderSide: BorderSide(width: 3, color: Colors.greenAccent),
+                          //   ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(width: 3, color: Colors.purple),
+                            ),
+                            errorBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  width: 3, color: Color.fromARGB(255, 66, 125, 145)),
+                            ),
+                          
+                          
+                          ),
+                      controller: myPassController,
+                    ),
+            
+                    SizedBox(
+                      height: 10,
+                    ),
+            
+            
+                    Container(width: 150, child:TextButton(onPressed: (){}, child: Text("Log in", style: TextStyle(color: Colors.white),), style: ButtonStyle(
+                     
+                backgroundColor: MaterialStatePropertyAll<Color>(Colors.purple),
+              ),),)
+            
+            
+            
+                  ],
+                ),
+              ),
+            ),
+        ),
+      
+      
+    );
+  }
+}
+
+
+
+class CurvePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint();
+    paint.color = Colors.purple;
+    paint.style = PaintingStyle.fill;
+
+    var path = Path();
+
+    path.moveTo(0, size.height * 0.9167);
+    path.quadraticBezierTo(size.width * 0.25, size.height * 0.875,
+        size.width * 0.5, size.height * 0.9167);
+    path.quadraticBezierTo(size.width * 0.75, size.height * 0.9584,
+        size.width * 1.0, size.height * 0.9167);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:millennium_app/Screen/FrontScreen/FrontSlide.dart';
+import 'package:millennium_app/Screen/FrontScreen/LogInScreen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: showHome ? MyHomePage(title: "HEllo") : FrontSlider(),
+      home: showHome ? LogInScreen() : FrontSlider(),
     );
   }
 }
