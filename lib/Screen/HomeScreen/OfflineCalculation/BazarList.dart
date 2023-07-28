0e668576661c109432f1af4db9c21107c078e7cf
Developer20202021 <mahadikaushik8888@gmail.com>
2023-07-28 20:00:06 +0600

    done Bazar List

--- lib/Screen/HomeScreen/OfflineCalculation/BazarList.dart
+++ lib/Screen/HomeScreen/OfflineCalculation/BazarList.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 import 'package:flutter/material.dart';
 
 class BazarList extends StatefulWidget {
@@ -8,74 +9,25 @@ class BazarList extends StatefulWidget {
 }
 
 class _BazarListState extends State<BazarList> {
+  String UserType = "Admi";
+
   List BazarList = [
-    {"productName": "Product Name",
-      "Product Amount": "120 kg"
-    },
-
-     {"productName": "Product Name",
-      "Product Amount": "120 kg"
-    },
-
-     {"productName": "Product Name",
-      "Product Amount": "120 kg"
-    },
-
-     {"productName": "Product Name",
-      "Product Amount": "120 kg"
-    },
-
-     {"productName": "Product Name",
-      "Product Amount": "120 kg"
-    },
-
-     {"productName": "Product Name",
-      "Product Amount": "120 kg"
-    },
-
-
-     {"productName": "Product Name",
-      "Product Amount": "120 kg"
-    },
-
-
-     {"productName": "Product Name",
-      "Product Amount": "120 kg"
-    },
-
-     {"productName": "Product Name",
-      "Product Amount": "120 kg"
-    },
-
-     {"productName": "Product Name",
-      "Product Amount": "120 kg"
-    },
-
-
-     {"productName": "Product Name",
-      "Product Amount": "120 kg"
-    },
-
-     {"productName": "Product Name",
-      "Product Amount": "120 kg"
-    },
-
-     {"productName": "Product Name",
-      "Product Amount": "120 kg"
-    },
-
-
-     {"productName": "Product Name",
-      "Product Amount": "120 kg"
-    },
-
-
-     {"productName": "Product Name",
-      "Product Amount": "120 kg"
-    },
-     {"productName": "Product Name",
-      "Product Amount": "120 kg"
-    },
+    {"productName": "Product Name", "Product Amount": "120 kg"},
+    {"productName": "Product Name", "Product Amount": "120 kg"},
+    {"productName": "Product Name", "Product Amount": "120 kg"},
+    {"productName": "Product Name", "Product Amount": "120 kg"},
+    {"productName": "Product Name", "Product Amount": "120 kg"},
+    {"productName": "Product Name", "Product Amount": "120 kg"},
+    {"productName": "Product Name", "Product Amount": "120 kg"},
+    {"productName": "Product Name", "Product Amount": "120 kg"},
+    {"productName": "Product Name", "Product Amount": "120 kg"},
+    {"productName": "Product Name", "Product Amount": "120 kg"},
+    {"productName": "Product Name", "Product Amount": "120 kg"},
+    {"productName": "Product Name", "Product Amount": "120 kg"},
+    {"productName": "Product Name", "Product Amount": "120 kg"},
+    {"productName": "Product Name", "Product Amount": "120 kg"},
+    {"productName": "Product Name", "Product Amount": "120 kg"},
+    {"productName": "Product Name", "Product Amount": "120 kg"},
   ];
 
   @override
@@ -99,13 +51,76 @@ class _BazarListState extends State<BazarList> {
           child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Column(children: [
-        
-            for(var item in BazarList)
-                ListTile(
-                  leading: Icon(Icons.store),
-                  title: Text("Product Name"),
-                  trailing: Text("140kg"),
-                ),
+                for (var item in BazarList)
+                  Column(
+                    children: [
+                      ListTile(
+                        leading: Icon(Icons.store),
+
+                        subtitle: UserType =="Admin"?Text("10"):  TextField(
+
+                          keyboardType: TextInputType.number,
+                        
+                 
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Product price',
+                            
+                            hintText: 'Product price',
+                            
+                            //  enabledBorder: OutlineInputBorder(
+                            //       borderSide: BorderSide(width: 3, color: Colors.greenAccent),
+                            //     ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(width: 3, color: Colors.purple),
+                                ),
+                                errorBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      width: 3, color: Color.fromARGB(255, 66, 125, 145)),
+                                ),
+                            
+                            
+                            ),
+                 
+                      ),
+                      // { UserType == "Admin"? subtitle: Text("Product Name"):subtitle: }
+                        title: Text("Product Name"),
+                        trailing: Text("140kg"),
+                      ),
+                         Container(width: 150, child:TextButton(onPressed: (){
+
+
+                           final snackBar = SnackBar(
+                  /// need to set following properties for best effect of awesome_snackbar_content
+                  elevation: 0,
+                  behavior: SnackBarBehavior.floating,
+                  backgroundColor: Colors.transparent,
+                  content: AwesomeSnackbarContent(
+                    title: 'Successfully Added',
+                    message:
+                        'Your Product Price is Added Successfully',
+
+                    /// change contentType to ContentType.success, ContentType.warning or ContentType.help for variants
+                    contentType: ContentType.success,
+                  ),
+                );
+
+                ScaffoldMessenger.of(context)
+                  ..hideCurrentSnackBar()
+                  ..showSnackBar(snackBar);
+
+
+
+
+                         }, child: Text("Add", style: TextStyle(color: Colors.white),), style: ButtonStyle(
+                   
+              backgroundColor: MaterialStatePropertyAll<Color>(Colors.purple),
+                          ),),),
+                      Divider(thickness: 2, height: 30,color: Colors.purple,)
+                    ],
+                  ),
+
+                  
               ])),
         ));
   }
